993fef52bae95d767f6c4e9736f70718cfacb3ea
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-02-23 00:15:15 +0100

    implemented trainer schema

---
+++ src/api/trainer/controllers/trainer.ts
@@ -0,0 +1,7 @@
+/**
+ * trainer controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::trainer.trainer');
---
+++ src/api/trainer/services/trainer.ts
@@ -0,0 +1,7 @@
+/**
+ * trainer service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::trainer.trainer');
