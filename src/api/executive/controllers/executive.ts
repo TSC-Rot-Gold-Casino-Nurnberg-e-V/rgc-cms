b60b0611edc390a9968662f98ac692d44ac5bfb4
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-02-23 22:51:53 +0100

    implemented executive schema

---
+++ src/api/executive/controllers/executive.ts
@@ -0,0 +1,7 @@
+/**
+ * executive controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::executive.executive');
---
+++ src/api/executive/services/executive.ts
@@ -0,0 +1,7 @@
+/**
+ * executive service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::executive.executive');
