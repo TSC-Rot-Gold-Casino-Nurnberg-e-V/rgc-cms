db6cead7fd7dff543e597b0c926b815d79e04673
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-07-09 12:06:22 +0200

    added property "lizenzen" to trainer
    https://github.com/TSC-Rot-Gold-Casino-Nurnberg-e-V/rgc-website/issues/44

---
+++ src/api/lizenz/routes/lizenz.ts
@@ -0,0 +1,7 @@
+/**
+ * lizenz router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::lizenz.lizenz');
---
+++ src/api/lizenz/services/lizenz.ts
@@ -0,0 +1,7 @@
+/**
+ * lizenz service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::lizenz.lizenz');
