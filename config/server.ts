66870e0e342186c4ba25d3b354ccdbdcc3b62f9f
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2023-07-17 15:56:28 +0200

    migration (v4.5.1+ to v4.6.1)

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    // Add this to not receive populated relations in webhooks
+    populateRelations: false,
   },
 });
