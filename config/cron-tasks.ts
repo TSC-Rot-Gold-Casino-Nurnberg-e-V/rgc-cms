ae515557a861883c1240b55f5915d4cf7e862d9d
vitalia <vitalia.svirskaja.vs@gmail.com>
2023-08-22 07:56:36 +0200

    improved logging of cron job (build frontend)

--- config/cron-tasks.ts
+++ config/cron-tasks.ts
@@ -4,9 +4,16 @@ import { env } from "@strapi/utils";
 export default {
   buildFrontend: {
     task: async () => {
-      console.log("Building frontend...");
-      const url = env("VERCEL_DEPLOYMENT_WEBHOOK_URL", "");
-      await axios.post(url);
+      try {
+        const url = env("VERCEL_DEPLOYMENT_WEBHOOK_URL", "");
+        console.log("calling deployment webhook with url: ", url);
+        const response = await axios.post(url);
+        console.log("webhook triggered successfully");
+        console.log("response.status: ", response.status);
+        console.log("response.data: ", response.data);
+      } catch (e) {
+        console.error("error calling deployment webhook: ", e);
+      }
     },
     options: {
       // every day at 03:00
