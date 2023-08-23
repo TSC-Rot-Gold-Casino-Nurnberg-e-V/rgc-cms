80b243ea890150be2e68b8ea909222cac849a923
vitalia <vitalia.svirskaja.vs@gmail.com>
2023-08-23 22:14:04 +0200

    added logic for preventing cold start errors when triggering frontend build

--- config/cron-tasks.ts
+++ config/cron-tasks.ts
@@ -1,9 +1,31 @@
 import axios from "axios";
 import { env } from "@strapi/utils";
 
+async function wakeUpCMS() {
+  const MAX_RETRIES = 5;
+  const CMS_URL = env("CMS_URL", "");
+  let retries = 0;
+  let success = false;
+  while (!success && retries < MAX_RETRIES) {
+    try {
+      console.log("trying to wake up cms");
+      const response = await axios.get(`${CMS_URL}/admin`);
+      console.log("cms woke up successfully");
+      console.log("response.status: ", response.status);
+      console.log("response.data: ", response.data);
+      success = true;
+    } catch (e) {
+      console.error("error waking up cms: ", e);
+      retries++;
+    }
+  }
+}
+
 export default {
   buildFrontend: {
     task: async () => {
+      await wakeUpCMS();
+
       try {
         const url = env("VERCEL_DEPLOYMENT_WEBHOOK_URL", "");
         console.log("calling deployment webhook with url: ", url);
