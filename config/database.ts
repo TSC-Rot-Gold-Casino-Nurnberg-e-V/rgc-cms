a5301cfd74b2ed122ee55d4530c25f6b279c3ff9
vitalia <vitalia.svirskaja.vs@gmail.com>
2023-08-24 22:14:13 +0200

    prettier

--- config/database.ts
+++ config/database.ts
@@ -1,14 +1,14 @@
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'rgc-cms'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'banane'),
-      ssl: env.bool('DATABASE_SSL', false),
-      connectionString: env('DATABASE_URL', ''),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "rgc-cms"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "banane"),
+      ssl: env.bool("DATABASE_SSL", false),
+      connectionString: env("DATABASE_URL", ""),
     },
   },
 });
